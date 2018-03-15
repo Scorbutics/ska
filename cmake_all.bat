a22e461c876684f3eb9f11f63f9fb1f148bd1a8e
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-03-15 19:01:01 +0100

    Scripts CMake : suite

---
+++ cmake_all.bat
@@ -0,0 +1,27 @@
+set target=%1
+
+cd ska_core
+call cmake_.bat %target%
+cd ../ska_core_test
+call cmake_.bat %target%
+cd ../ska_draw
+call cmake_.bat %target%
+cd ../ska_gui
+call cmake_.bat %target%
+cd ../ska_gui_test
+call cmake_.bat %target%
+cd ../ska_physic
+call cmake_.bat %target%
+cd ../ska_physic_chipmunk
+call cmake_.bat %target%
+cd ../ska_physic_chipmunk_test
+call cmake_.bat %target%
+cd ../ska_physic_homemade
+call cmake_.bat %target%
+cd ../ska_physic_simulation
+call cmake_.bat %target%
+cd ../ska_script
+call cmake_.bat %target%
+cd ../ska_world
+call cmake_.bat %target%
+cd ..
---
+++ make_all.bat
@@ -0,0 +1,19 @@
+cd ska_core
+mingw32-make
+cd ../ska_core_test
+mingw32-make
+cd ../ska_draw
+mingw32-make
+cd ../ska_gui
+mingw32-make
+cd ../ska_gui_test
+mingw32-make
+cd ../ska_physic
+mingw32-make
+cd ../ska_physic_chipmunk
+mingw32-make
+cd ../ska_physic_chipmunk_test
+mingw32-make
+cd ../ska_script
+mingw32-make
+cd ..
