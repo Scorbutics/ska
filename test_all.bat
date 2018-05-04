95f1cd81c669da3030885a6915ff42685358d10c
Scorbutics <brice.bulgarelli@gmail.com>
2018-05-04 23:35:07 +0200

    Modification script batch de lancement des tests

--- test_all.bat
+++ test_all.bat
@@ -2,17 +2,14 @@ set PROJECT_PATH=%1
 set PROJECT_PATH=%PROJECT_PATH:"=%
 
 copy %PROJECT_PATH%\Debug\ska_core_test.exe %PROJECT_PATH%
-cd %PROJECT_PATH%
-ska_core_test.exe
-
 copy %PROJECT_PATH%\Debug\ska_ai_test.exe %PROJECT_PATH%
-cd %PROJECT_PATH%
-ska_ai_test.exe
-
 copy %PROJECT_PATH%\Debug\ska_gui_test.exe %PROJECT_PATH%
-cd %PROJECT_PATH%
-ska_gui_test.exe
-
 copy %PROJECT_PATH%\Debug\ska_world_test.exe %PROJECT_PATH%
 cd %PROJECT_PATH%
+
+ska_core_test.exe
+ska_ai_test.exe
+ska_gui_test.exe
 ska_world_test.exe
+
+exit /b %errorlevel%
