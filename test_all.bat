4a9ad0e74f79aca782cba9962625669f2e656d18
Scorbutics <brice.bulgarelli@outlook.com>
2019-05-02 00:33:38 +0200

    chore : script file & gitignore

--- test_all.bat
+++ test_all.bat
@@ -1,15 +1,10 @@
-set PROJECT_PATH=%1
-set PROJECT_PATH=%PROJECT_PATH:"=%
-
-copy %PROJECT_PATH%\Debug\ska_core_test.exe %PROJECT_PATH%
-copy %PROJECT_PATH%\Debug\ska_ai_test.exe %PROJECT_PATH%
-copy %PROJECT_PATH%\Debug\ska_gui_test.exe %PROJECT_PATH%
-copy %PROJECT_PATH%\Debug\ska_world_test.exe %PROJECT_PATH%
-cd %PROJECT_PATH%
+cd bin/Debug
 
 ska_core_test.exe
 ska_ai_test.exe
 ska_gui_test.exe
 ska_world_test.exe
+
+cd ..
 
 exit /b %errorlevel%
