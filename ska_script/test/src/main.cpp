eff16b5a4b1d1297357feff7543a919269954f90
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2019-07-02 09:55:34 +0200

    test : adding unit-test folder & cmake target

---
+++ ska_script/test/src/main.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	doctest::Context context = doctest::Context{argc, argv};
+	auto resultCode = 0;
+
+	// sort the test cases by their name
+	context.setOption("order-by", "name");
+	context.setOption("success", "true");
+	resultCode = context.run();
+
+	if (context.shouldExit() || resultCode != 0) {
+		return(-1);
+	}
+
+	return resultCode;
+}
