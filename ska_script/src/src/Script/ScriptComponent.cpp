e4dc168d2e92a2b153ecfa3d0292433c627362b4
Scorbutics <brice.bulgarelli@gmail.com>
2019-07-15 20:41:15 +0200

    refactor: exponential => square

--- ska_script/src/src/Script/ScriptComponent.cpp
+++ ska_script/src/src/Script/ScriptComponent.cpp
@@ -34,55 +34,54 @@ float ska::ScriptComponent::getPriority(const unsigned int currentTimeMillis) co
     return priorityFromElapsedTime;
   } 
 
-  /* A PAUSED script must gain rapidly high priority contrary to a STOPPED one.
-  Then we use the exponential to simulate that */
-  return NumberUtils::exponential(priorityFromElapsedTime);
+  /* A PAUSED script must gain rapidly high priority contrary to a STOPPED one */
+    return priorityFromElapsedTime * priorityFromElapsedTime;
 }
 
 bool ska::ScriptComponent::canBePlayed() const {
-  switch(m_state) {
-    case ScriptState::RUNNING:
-    case ScriptState::DEAD:
-      return false;
-    
-    case ScriptState::STOPPED:
-      return m_triggeringType == ScriptTriggerType::AUTO;
+switch(m_state) {
+case ScriptState::RUNNING:
+case ScriptState::DEAD:
+  return false;
 
-    default:
-      return (TimeUtils::getTicks() - m_lastTimeDelayed) <= m_delay;
-  }
+case ScriptState::STOPPED:
+  return m_triggeringType == ScriptTriggerType::AUTO;
+
+default:
+  return (TimeUtils::getTicks() - m_lastTimeDelayed) <= m_delay;
+}
 }
 
 ska::ScriptState ska::ScriptComponent::updateState() {
-    switch(m_state) {
-    case ScriptState::PAUSED:
-        if(TimeUtils::getTicks() - m_lastTimeDelayed  > m_delay) {
-            m_state = ScriptState::RUNNING;
-            m_delay = 0;
-        }
-        break;
-
-    case ScriptState::STOPPED:
-        if (m_triggeringType == ScriptTriggerType::AUTO) {
-            //auto reload
-            m_state = ScriptState::PAUSED;
-            m_lastTimeDelayed = TimeUtils::getTicks();
-        } 
-        break;
-
-    default:
+switch(m_state) {
+case ScriptState::PAUSED:
+    if(TimeUtils::getTicks() - m_lastTimeDelayed  > m_delay) {
         m_state = ScriptState::RUNNING;
-        break;
+        m_delay = 0;
     }
+    break;
 
-	return m_state;
+case ScriptState::STOPPED:
+    if (m_triggeringType == ScriptTriggerType::AUTO) {
+        //auto reload
+        m_state = ScriptState::PAUSED;
+        m_lastTimeDelayed = TimeUtils::getTicks();
+    } 
+    break;
+
+default:
+    m_state = ScriptState::RUNNING;
+    break;
+}
+
+return m_state;
 }
 
 bool ska::ScriptComponent::play(Interpreter& interpreter) {
-    /* Update status once */
-    if (updateState() == ScriptState::PAUSED) {
-        return false;
-    }
+/* Update status once */
+if (updateState() == ScriptState::PAUSED) {
+    return false;
+}
 
     m_lastTimeStarted = TimeUtils::getTicks();
     try {	
